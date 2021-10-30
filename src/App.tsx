11c9f531b6a56c8208b610703807402dd88de988
vcpersike <vcpersike@gmail.com>
2021-10-30 14:15:44 -0300

    teste 3

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Teste <code>Teste 2</code> and save to reload.
+          Teste <code>Teste 2</code> teste 3 de upload.
         </p>
         <a
           className="App-link"
